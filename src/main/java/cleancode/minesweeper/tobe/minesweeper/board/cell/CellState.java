5b29fa24aaa879c82443e240fad02e72132c64d1
Bombo-Dev <bombo96@chungwoon.ac.kr>
2024-10-16 22:23:43 +0900

    refactor : 빠진 fileChange 추가

--- src/main/java/cleancode/minesweeper/tobe/minesweeper/board/cell/CellState.java
+++ src/main/java/cleancode/minesweeper/tobe/minesweeper/board/cell/CellState.java
@@ -19,10 +19,6 @@ public class CellState {
     }
 
     public void open() {
-        if (this.isFlagged) {
-            this.isFlagged = false;
-        }
-
         this.isOpened = true;
     }
 
